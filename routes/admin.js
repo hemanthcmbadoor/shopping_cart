81838fcf662d25787ad597a463b99c80a123be7d
Hemanth C M <badoorhemanthcm@gmail.com>
2021-09-23 10:20:33 +0530

    add product

--- routes/admin.js
+++ routes/admin.js
@@ -1,28 +1,32 @@
 var express = require('express');
+const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
 var productHelper = require('../helpers/product-helpers');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('admin/view-products',{ admin:true });
-});
+router.get('/', function (req, res, next) {
 
-router.get('/add-product',function(req,res){
-  res.render('admin/add-product',{ admin:true });
-});
-
-router.post('/add-product',(req,res)=>{
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products)
+    res.render('admin/view-products', { admin: true });
+  })
   
-  productHelper.addProduct(req.body,(id)=>{
-    let image_file=req.files.image
-    image_file.mv('./public/images/product-images/'+id+'.jpg',(err,done)=>{
-      if(!err){
+});
+
+router.get('/add-product', function (req, res) {
+  res.render('admin/add-product', { admin: true });
+});
+
+router.post('/add-product', (req, res) => {
+  productHelper.addProduct(req.body, (id) => {
+    let image_file = req.files.image
+    image_file.mv('./public/images/product-images/' + id + '.jpg', (err, done) => {
+      if (!err) {
         res.render("admin/add-product")
       }
-      else{
+      else {
         console.log(err)
       }
     })
-    
   })
 });
 
