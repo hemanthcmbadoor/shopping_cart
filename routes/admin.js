50d76bf8b6b65e02c39029b1d2e9780b29331125
Hemanth C M <badoorhemanthcm@gmail.com>
2021-09-23 14:41:03 +0530

    user header

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,11 @@
 var express = require('express');
 const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
-var productHelper = require('../helpers/product-helpers');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-
-  productHelpers.getAllProducts().then((products)=>{
-    console.log(products)
-    res.render('admin/view-products', { admin: true });
+  productHelpers.getAllProducts().then((products) => {
+    res.render('admin/view-products', { admin: true, products });
   })
-  
 });
 
 router.get('/add-product', function (req, res) {
@@ -17,7 +13,7 @@ router.get('/add-product', function (req, res) {
 });
 
 router.post('/add-product', (req, res) => {
-  productHelper.addProduct(req.body, (id) => {
+  productHelpers.addProduct(req.body, (id) => {
     let image_file = req.files.image
     image_file.mv('./public/images/product-images/' + id + '.jpg', (err, done) => {
       if (!err) {
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+const productHelpers = require('../helpers/product-helpers');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { admin:false });
+router.get('/', function (req, res, next) {
+  productHelpers.getAllProducts().then((products) => {
+    res.render('user/view-products', { admin: false, products });
+  })
+
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('user/login');
 });
 
 module.exports = router;
